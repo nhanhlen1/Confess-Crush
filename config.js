41e62e3a92517ff717458aae6170186a28d5e735
nhanhlen1 <91404191+nhanhlen1@users.noreply.github.com>
2023-11-07 00:56:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     titleWeb: "Điều đặc biệt to to",
-    introTitle: 'Cún àaaaaaa',
-    introDesc: 'Ngồi lại đây mình nói nhỏ với Cún một xíu nèeee',
-    btnIntro: 'Khoảng thời gian vừa qua Lu đã rất vui vì luôn có Cún ở bên cạnh 😘😘',
-    title: 'Nhưng mà Lu hơi ích kỉ một xíu, Lu chỉ muốn Cún là của riêng Lu thôi 🥺',
-    desc: 'Nên là... Cún làm bạn gái Lu nhéeee ',
+    introTitle: 'Ling àaaaaaa',
+    introDesc: 'Ngồi lại đây mình nói nhỏ với Ling một xíu nèeee',
+    btnIntro: 'Khoảng thời gian vừa qua Lu đã rất vui vì luôn có Ling ở bên cạnh 😘😘',
+    title: 'Nhưng mà Lu hơi ích kỉ một xíu, Lu chỉ muốn Ling là của riêng Lu thôi 🥺',
+    desc: 'Nên là... Ling làm bạn gái Lu nhéeee ',
     btnYes: 'Được nè <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Từ nay bạn là của mình rồi á :3 Mình sẽ chịu trách nhiệm với bạn nhaaa. Nếu mà nhớ mình thì nhắn tin cho mình nhé <3 Mình luôn ở đâyyyy ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100038845686262' 
+    messLink: 'https://youtube.com/shorts/ZQl0850Ayuo?si=yeptZYKrCkewRbRj' 
 }
